adbb2c5963ba5b1b7af0016b3fc3ecb1f0453793
Atangyl <1256354282@qq.com>
2025-12-05 17:58:07 +0800

    [remove] remove correct polygon code

--- widget/src/draw_panel_gal.cpp
+++ widget/src/draw_panel_gal.cpp
@@ -147,17 +147,17 @@ void DrawPanelGal::InitialViewData(DataManager* data)
 	m_gal->SetLineWidth(m_view->ToWorld(1));
 	//m_gal->SetIsFill(true);
 	//m_gal->SetFillColor(KIGFX::COLOR4D(1, 1, 1, 1));
-	//for (auto &circle : data->m_circles) {
-	//	circle.m_centerPoint = m_view->ToWorld(circle.m_centerPoint);
-	//	circle.m_radius = m_view->ToWorld(circle.m_radius);
-	//	m_view->Add(&circle);
-	//}
+	for (auto &circle : data->m_circles) {
+		circle.m_centerPoint = m_view->ToWorld(circle.m_centerPoint);
+		circle.m_radius = m_view->ToWorld(circle.m_radius);
+		m_view->Add(&circle);
+	}
 
-	//for (auto& rectangle : data->m_rectangles) {
-	//	rectangle.m_startPoint = m_view->ToWorld(rectangle.m_startPoint);
-	//	rectangle.m_endPoint = m_view->ToWorld(rectangle.m_endPoint);
-	//	m_view->Add(&rectangle);
-	//}
+	for (auto& rectangle : data->m_rectangles) {
+		rectangle.m_startPoint = m_view->ToWorld(rectangle.m_startPoint);
+		rectangle.m_endPoint = m_view->ToWorld(rectangle.m_endPoint);
+		m_view->Add(&rectangle);
+	}
 	
 	for (int i = 0; i < data->m_polygons.size(); i++) {
 		auto& polygon = data->m_polygons[i];
