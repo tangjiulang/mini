82969363510c21780eae5f668bd6fb0959ab0bd1
Atangyl <1256354282@qq.com>
2026-03-02 11:42:05 +0800

    [feature] Change spdlog to asynchronous mode

--- mini.cpp
+++ mini.cpp
@@ -2,12 +2,15 @@
 #include "mini_frame.hxx"
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/async.h>
 
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
 
-    auto file_logger = spdlog::basic_logger_mt("default", "log.txt", true);
+    spdlog::init_thread_pool(8192, 1);
+
+    auto file_logger = spdlog::basic_logger_mt<spdlog::async_factory>("default", "log.txt", true);
     spdlog::set_default_logger(file_logger);
 
     spdlog::set_level(spdlog::level::level_enum::trace);
